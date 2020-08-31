0a56da9b33b1138ba5e97ff6b7ad43c13e9c9183
Joel Bernstein <bergamot@gmail.com>
2020-08-30 20:47:37 -0700

    Initial Commit

---
+++ Shared/FiveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiveyApp.swift
+//  Shared
+//
+//  Created by Joel Bernstein on 8/30/20.
+//
+
+import SwiftUI
+
+@main
+struct FiveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
