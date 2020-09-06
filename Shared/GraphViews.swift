5a527958f0fb1e347345a8478f705779cf377195
Joel Bernstein <bergamot@gmail.com>
2020-09-05 21:20:06 -0700

    Added iOS application icons and minor grid lines

--- Shared/GraphViews.swift
+++ Shared/GraphViews.swift
@@ -41,6 +41,40 @@ struct GraphMajorGridLines: Shape
     }
 }
 
+struct GraphMinorGridLines: Shape
+{
+    let poll: Poll?
+    
+    func path(in rect: CGRect) -> Path {
+
+        var path = Path()
+
+        let data = poll?.candidates.first?.dataPoints.reversed() ?? []
+        
+        for (index, dataPoint) in data.enumerated() {
+            guard let date = dataPoint.date else { continue }
+            
+            let x = rect.minX + (CGFloat(index) / CGFloat(data.count - 1)) * rect.width
+            let day = Calendar.autoupdatingCurrent.component(.weekday, from: date)
+            
+            if day == 1 && index != 0 && index != (data.count - 1) {
+                path.move(to: CGPoint(x: x, y: rect.minY))
+                path.addLine(to: CGPoint(x: x, y: rect.maxY))
+            }
+        }
+        
+        for percent: CGFloat in stride(from: 0.1, through: 0.9, by: 0.1) {
+            let y = rect.minY + rect.height * percent
+            
+            path.move(to: CGPoint(x: rect.minX, y: y))
+            path.addLine(to: CGPoint(x: rect.maxX, y: y))
+        }
+
+        
+        return path
+    }
+}
+
 //struct StatRangeGraphShape: Shape
 //{
 //    let candidate: Candidate?
@@ -164,10 +198,13 @@ struct WinProbabilityGraph: View
 {
     let poll: Poll?
 
+    
+    
     var body: some View {
         ZStack {
+            GraphMinorGridLines(poll: poll).stroke(Color(UIColor.secondarySystemGroupedBackground).opacity(0.4), lineWidth: 1)
             GraphMajorGridLines(poll: poll).stroke(Color(UIColor.secondarySystemGroupedBackground), lineWidth: 1)
-            
+
             WinProbabilityGraphLine(candidate: poll?.candidate(named: "Biden")).stroke(Color.blue, lineWidth: 2)
             WinProbabilityGraphLine(candidate: poll?.candidate(named: "Trump")).stroke(Color.red, lineWidth: 2)
             
